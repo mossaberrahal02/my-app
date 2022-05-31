a503f5307a65740ec2a6545abbdf204c71bad6de
mossab-errahal <mossaberrahal@gmail.com>
2022-05-31 14:51:50 +0100

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';//khas t importer reactdom mn 'react-dom/client' darori hadi hiya li kayna f version jdidda 
+import App from './App';
+
+//ReactDOM.render(<h1>test render</h1>,document.getElementById('root'));  !!! had l methode 9damet f version 9dima
+//render has 2 parametres render(what, where)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));//daba khedamin b had l methode 
+root.render(
+    <App/>
+);//l parametre what khas tkon kamla f west men balise wehda
+
+
+
+/*
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+*/
